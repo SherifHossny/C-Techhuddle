82001d4a06ba143eccfecede93aa8dad79ff0356
SherifHossny <46594717+SherifHossny@users.noreply.github.com>
2020-02-25 23:36:22 +0200

    Add files via upload

---
+++ 24.2.20h/24.2.20h/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _24._2._20h
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Height= ");
+            var h = double.Parse(Console.ReadLine());
+            Console.Write("Side= ");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area of the triangle= " + h * (b / 2));
+        }
+    }
+}
